e902837411b073ea8311e431f7ebe98033ef537a
varunsetlam3 <166518748+varunsetlam3@users.noreply.github.com>
2024-04-15 13:24:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => res.send("This response is coming from source deployment code"));
+app.get("/", (req, res) => res.send("This response is coming from source deployment code, please check for the latest changes "));
 
 let server = app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
